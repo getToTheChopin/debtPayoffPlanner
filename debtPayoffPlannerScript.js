149720819df5631adb038092b230e5a8e334e72f
getToTheChopin <alan.ang@mail.mcgill.ca>
2019-08-24 11:21:10 -0400

    Add copy to clipboard function

--- debtPayoffPlannerScript.js
+++ debtPayoffPlannerScript.js
@@ -1484,3 +1484,23 @@ function displayMoreInfo(){
     }
 
 }
+
+function copyToClipboard(containerid) {
+    if (screen.width >= 600) {
+        if (document.selection) { 
+            var range = document.body.createTextRange();
+            range.moveToElementText(document.getElementById(containerid));
+            range.select().createTextRange();
+            document.execCommand("copy"); 
+        
+        } else if (window.getSelection) {
+            var range = document.createRange();
+            range.selectNode(document.getElementById(containerid));
+            window.getSelection().addRange(range);
+            document.execCommand("copy");
+        }
+    }
+    else {
+        return;
+    }
+}
